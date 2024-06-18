2c634c9b3dd1c040d4248e07b411219b239ed075
Felipe Matheus Mendes Mori <fmatheusog@gmail.com>
2024-06-18 14:54:31 -0300

    feat: implements main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <main className="">Hello World</main>;
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-64 bg-zinc-900 p-6">Sidebar</aside>
+        <main className="flex-1 p-6">Main</main>
+      </div>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
+        Footer
+      </footer>
+    </div>
+  );
 }
